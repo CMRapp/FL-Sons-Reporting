09ae00f8a594d8ce3381e40350eb1e0d7e37fee0
Christian M Rapp <106698098+CMRapp@users.noreply.github.com>
2025-03-27 10:09:06 -0400

    revert to working version

--- next.config.js
+++ next.config.js
@@ -28,6 +28,7 @@ const nextConfig = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   // Ensure CSS is properly handled in production
   webpack: (config) => {
     config.module.rules.push({
@@ -36,6 +37,8 @@ const nextConfig = {
     });
     return config;
   },
+=======
+>>>>>>> parent of 7b9ebff (fixed style-loader issue)
 =======
 >>>>>>> parent of 7b9ebff (fixed style-loader issue)
 =======
